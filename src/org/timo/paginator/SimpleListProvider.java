7be4ae35c13b3a6cadb9b080fbc802d623871006
Timoteo Ponce <timoteo.ponce@swissbytes.ch>
2011-07-21 11:32:58 -0400

    SimpleListProvider class added

---
+++ src/org/timo/paginator/SimpleListProvider.java
@@ -0,0 +1,40 @@
+
+package org.timo.paginator;
+
+import java.util.List;
+/**
+ *
+ * @author Timoteo Ponce
+ */
+public class SimpleListProvider<T> implements ListProvider<T> {
+    private final List<T> sourceList;
+
+    /**
+     * @param sourceList
+     */
+    public SimpleListProvider(final List<T> sourceList) {
+        if (sourceList == null) {
+            throw new IllegalArgumentException("null lists are not allowed");
+        }
+        this.sourceList = sourceList;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.timo.paginator.ListProvider#provideList(org.timo.paginator.SegmentProvider)
+     */
+    @Override
+    public List<T> provideList(final SegmentProvider provider) {
+        Segment segment = provider.getSegment(sourceList.size());
+        return truncateList(sourceList, segment.getFromIndex(), segment.getSize());
+    }
+
+    List<T> truncateList(final List<T> list, final int startingPoint, final int chunkSize) {
+        int lastIndex = startingPoint + chunkSize;
+        if (lastIndex > list.size() || chunkSize <= 0) {
+            lastIndex = list.size();
+        }
+        return list.subList(startingPoint, lastIndex);
+    }
+}
